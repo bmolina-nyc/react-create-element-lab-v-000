00fa16bed225ca153b7aabb2e234a8cacb8028cb
bmolina-nyc <bruce.molina.81@gmail.com>
2017-03-14 20:06:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 //Code React element here
+
+
+
+const meInReact = 
+  React.createElement('div', {className:'me'},
+    React.createElement('h1', {}, "An Awesome Person"),
+    React.createElement('p', {}, "Who is learning React"),
+
+  React.createElement('ul', {className: 'me_interests'},
+    [
+      React.createElement('li', {className: 'JS'}, "JavaScript"),
+      React.createElement('li', {className: 'React'}, "React"),
+      React.createElement('li', {className: 'Movies'}, "Movies"), 
+      React.createElement('li', {className: 'Snacks'}, "Ice cream")
+    ]
+  ) // 'ul end' 
+) // 'div end'
+
+
+  ReactDOM.render(meInReact, document.getElementById('main'));
